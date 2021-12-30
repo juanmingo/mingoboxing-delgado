3b635544c4b50e8e3d13bbd682ac6e4571f405d2
Juan Delgado <juan.delgador@gmail.com>
2021-12-29 21:53:31 -0300

    Ini Tienda Mingo Boxing

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Mingo Boxing
+        </p>
+        <a
+          className="App-link"
+          href="https://github.com/juanmingo/mingoboxing-delgado"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+        Tienda
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
